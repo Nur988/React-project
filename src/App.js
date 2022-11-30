2c57d955c5cda02cd94b64f9e94d84c00590e556
Nur988 <proteas988@gmail.com>
2022-12-01 00:36:04 +0600

    chatbot

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import logo from './logo.svg';
-import './style.css';
+import React from 'react';
+import Chatbot from 'react-chatbot-kit'
+import './App.css';
+
+import ActionProvider from './ActionProvider';
+import MessageParser from './MessageParser';
+import config from './Config';
 
 function App() {
   return (
-    <div className="calculator">
-     <div className='output'>
-     <div className='previous-output'>123456</div>
-     <div className='current-output'>45678</div> 
-     </div>
-     <button className='span-two' >AC</button>
-     <button>DEL</button>
-     <button className='button' >+</button>
-     <button className='button' >-</button>
-     <button className='button' >%</button>
-     <button className='button' >1</button>
-     <button className='button' >2</button>
-     <button className='button' >3</button>
-     <button className='button' >4</button>
-     <button className='button' >5</button>
-     <button className='button' >6</button>
-     <button className='button' >7</button>
-     <button className='button' >8</button>
-     <button className='button' >9</button>
-     <button className='button' >0</button>
-     <button className='span-two' >=</button>
+    <div className="App">
+      <header className="App-header">
+        <Chatbot config={config} actionProvider={ActionProvider} 	    messageParser={MessageParser} />
+      </header>
     </div>
   );
 }
